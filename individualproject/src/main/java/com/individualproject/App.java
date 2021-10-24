771605cfac4b62f7bf20007fc9282a2df6966a64
Alexandre Chouman <alexandre.chouman@outlook.com>
2021-10-24 21:12:50 +0100

    add Maven

---
+++ individualproject/src/main/java/com/individualproject/App.java
@@ -0,0 +1,13 @@
+package com.individualproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/main/java/TestStuff/BasicStuff.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.TestStuff;
-
-public class BasicStuff {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
