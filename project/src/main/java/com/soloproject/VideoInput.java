ff94d7e854fd2fc5d90f0842ac9496f2d2741a50
Alexandre Chouman <alexandre.chouman@outlook.com>
2022-01-30 18:00:57 +0000

    try out sphinx4

--- project/src/main/java/com/soloproject/VideoInput.java
+++ project/src/main/java/com/soloproject/VideoInput.java
@@ -2,10 +2,21 @@ package com.soloproject;
 
 import java.io.File;
 
+import org.openimaj.image.MBFImage;
+import org.openimaj.video.VideoDisplay;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.result.WordResult;
 
 /**
  * A class to allow a user to select the video file they want analysed by
@@ -25,12 +36,34 @@ public class VideoInput {
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Select a Meeting recording");
         File video = chooser.showOpenDialog(stage);
-        if (video != null) {
-
+        try {
+            getSpeech(video);
+        } catch (Exception e) {
+            System.out.println("something went wrong with the video");
         }
+
     }
 
     public Button getButton() {
         return button;
+    }
+
+    public String getSpeech(File video) throws Exception {
+
+        InputStream videostream = new FileInputStream(video);
+
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+
+        recognizer.startRecognition(videostream);
+        recognizer.stopRecognition();
+        SpeechResult result = recognizer.getResult();
+        System.out.println(result.getHypothesis());
+        return result.getHypothesis();
     }
 }
