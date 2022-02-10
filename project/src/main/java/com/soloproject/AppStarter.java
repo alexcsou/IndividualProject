9d2db821e04ab0ec28cd3a51a3a41ab841ec04c7
Alex <alexandre.chouman@outlook.com>
2022-02-10 23:27:50 +0000

    test commit from second device

--- project/src/main/java/com/soloproject/AppStarter.java
+++ project/src/main/java/com/soloproject/AppStarter.java
@@ -4,7 +4,7 @@ package com.soloproject;
  * This is a starter class that works as a workaround to a quirk of Maven used
  * with JavaFX, which requires the main method to be outside the class that
  * contains the launch() method. Run this class to run the app, not the App
- * class.
+ * class. 
  */
 public class AppStarter {
     public static void main(final String[] args) {
