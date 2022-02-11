b03a4772886818bd1afecabd1c35b8252ecbb19b
Alex <alexandre.chouman@outlook.com>
2022-02-11 18:03:58 +0000

    work on transcriptView

---
+++ project/src/main/java/com/soloproject/TranscriptBubble.java
@@ -0,0 +1,14 @@
+package com.soloproject;
+
+/**
+ * A class that displays a line of text from the transcript as a standalone visual entity. Used in transcriptView.
+ */
+public class TranscriptBubble {
+
+    private TranscriptSentence sentence;
+
+    public TranscriptBubble(TranscriptSentence sentence){
+        
+    }
+    
+}
