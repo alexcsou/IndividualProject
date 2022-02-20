5ff4a6fe97648c652e3651877f2666704560e78e
Alexandre Chouman <alexandre.chouman@outlook.com>
2022-02-20 18:48:45 +0000

    finish styling transcript tab

--- project/src/main/java/com/soloproject/TranscriptBubble.java
+++ project/src/main/java/com/soloproject/TranscriptBubble.java
@@ -1,8 +1,11 @@
 package com.soloproject;
 
+import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * A class that displays a line of text from the transcript as a standalone
@@ -21,24 +24,40 @@ public class TranscriptBubble {
     public BorderPane getBubble() {
         BorderPane bubble = new BorderPane();
 
-        Label sentenceLabel = new Label(sentence.getSentence() + "\n\n");
+        Label sentenceLabel = new Label(sentence.getSentence());
         sentenceLabel.setMaxWidth(400);
         sentenceLabel.setMinWidth(100);
         sentenceLabel.setWrapText(true);
         sentenceLabel.setId("sentenceLabel");
 
+        Separator smallSep1 = new Separator(Orientation.HORIZONTAL);
+        // smallSep1.setId("bigSep");
+
+        Separator smallSep2 = new Separator(Orientation.HORIZONTAL);
+        // smallSep2.setId("bigSep");
+
+        VBox sentenceVBox = new VBox(smallSep1, sentenceLabel, smallSep2);
+        sentenceVBox.setSpacing(10);
+
         Label authorLabel = new Label(sentence.getAuthor() + ":\n\n\n");
         authorLabel.setId("authorLabel");
 
-        Label durationLabel = new Label(" Speech Duration: " + sentence.getDuration() + " seconds"); // cast to string
-        Label startAndEndLabel = new Label(sentence.getdurationString());
+        Label durationLabel = new Label("\n\n Speech Duration: " + sentence.getDuration() + " seconds"); // cast to
+                                                                                                         // string
+        durationLabel.setId("durationLabels");
 
-        HBox durationHBox = new HBox(durationLabel, startAndEndLabel);
+        Label startAndEndLabel = new Label("\n\n" + sentence.getdurationString());
+        startAndEndLabel.setId("durationLabels");
+
+        Label sep = new Label("\n\n  | ");
+        sep.setId("durationLabels");
+
+        HBox durationHBox = new HBox(durationLabel, sep, startAndEndLabel);
         durationHBox.setSpacing(30);
         durationHBox.setId("durationLabels");
 
         bubble.setTop(authorLabel);
-        bubble.setCenter(sentenceLabel);
+        bubble.setCenter(sentenceVBox);
         bubble.setBottom(durationHBox);
 
         if (style) {
